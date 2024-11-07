6cb6f1a6e0d35b56afe7e34a6816633d39f5e9fc
José Leonardo Zúñiga Hernández <leonidas12zh@gmail.com>
2024-11-06 23:12:16 -0600

    Seguridad completa yeeeeeiiiiii

--- SaludPlusSeguridad.sql
+++ SaludPlusSeguridad.sql
@@ -31,9 +31,17 @@ GO
 
 ---Permisos de modificacion
 
+GRANT EXECUTE ON dbo.Sp_ModificarProcedimiento
+TO Médico;
+GO
 
+GRANT EXECUTE ON dbo.Sp_ModificarSatisfaccion_Paciente 
+TO Médico;
+GO
 
-
+GRANT EXECUTE ON dbo.Sp_ModificarTipo_Procedimiento 
+TO Médico;
+GO
 
 
 ---Vistas
@@ -102,6 +110,46 @@ TO Recepcionista;
 GO
 
 --- Permisos de modificacion
+
+GRANT EXECUTE ON dbo.Sp_ModificarCita
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarFactura
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarHistorial_Medico
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarPlanificacion_Recurso
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarRecurso_Medico
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarPaciente
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarRecurso_Medico_Sala
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarProcedimiento
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarSatisfaccion_Paciente 
+TO Recepcionista;
+GO
+
+GRANT EXECUTE ON dbo.Sp_ModificarTipo_Procedimiento 
+TO Recepcionista;
+GO
 
 
 --- Vistas (Todas)
