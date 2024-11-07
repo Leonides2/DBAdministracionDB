8ee99d8478eb3f8507721483f07a0b3214f865f1
juanjosecorella <juanjosecorella2004@gmail.com>
2024-11-06 18:12:54 -0600

    Estilo

--- frontend-salud-plus/src/Components/Navbar.tsx
+++ frontend-salud-plus/src/Components/Navbar.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import {useNavigate } from "react-router-dom";
+
+import { useNavigate, matchPath, useLocation } from "react-router-dom";
 
 import './Navbar.css'
 
 const Navbar = () => {
-    const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
-
+    const { pathname } = useLocation();
     const navigate = useNavigate();
 
     // Define las rutas y etiquetas de los botones
@@ -31,35 +30,22 @@ const Navbar = () => {
         { path: "/planificaciones-recursos", label: "Planificaciones de Recursos" },
         { path: "/medico-planificaciones-recursos", label: "Medico-Planificaciones" },
         { path: "/satisfaccion-pacientes", label: "Evaluaciones de Satisfacción" },
-        { path: "/registrar-usuario", label: "Registro de Usuarios" },
+        { path: "/registro-usuario", label: "Registro de Usuarios" },
     ];
-
-    const handleButtonClick = (index: number) => {
-        setSelectedIndex(index);
-    };
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light p-3" >
             {
-            links.map((link, index) => (
-                
-                
-                    //<Link key={link.path} className="mr-2" to={link.path}>
-                
-                    <button
-                        type="button"
-                        className={`btn btn-sm ${selectedIndex === index ? "btn-primary" : "btn-outline-secondary"}`}
-                        onClick={() => {
-                            handleButtonClick(index)
-                            navigate(link.path)
-                        }}
-                    >
-                        {link.label}
-                    </button>
-                    
-                    //</Link>
-                    
-            ))}
+                links.map((link) => (
+                        <button
+                            type="button"
+                            className={`btn btn-sm me-2 ${matchPath(pathname, link.path) ? "btn-primary" : "btn-outline-secondary"}`}
+                            onClick={() => {navigate(link.path)}}
+                        >
+                            {link.label}
+                        </button>
+                ))
+            }
         </nav>
     );
 };
